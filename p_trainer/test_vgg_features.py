f664e7fca8b58616c4c24b662b3984e4737795e5
Chris Powers <chris.powers@berkeley.edu>
2017-06-14 10:50:44 -0700

    updated git ignore

--- p_trainer/test_vgg_features.py
+++ p_trainer/test_vgg_features.py
@@ -1,6 +1,11 @@
+import pickle
+from il_ros_hsr.tensor.nets.net_ycb import Net_YCB as Net
 from il_ros_hsr.tensor import inputdata
 from il_ros_hsr.p_pi.safe_corl.features import Features
 import numpy as np
+
+
+# data = pickle.load( open("rollouts/rollout0/rollout.p", "rb") )
 
 features = Features()
 

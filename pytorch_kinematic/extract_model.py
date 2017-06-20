0dec7180f1c16475a8108e7452d9036b562db146
Chris Powers <chris.powers@berkeley.edu>
2017-06-20 13:53:46 -0700

    created kinematic weight extraction code to pickle files

---
+++ pytorch_kinematic/extract_model.py
@@ -0,0 +1,172 @@
+import os
+import re
+import sys
+import cv2
+import math
+import time
+import scipy
+import argparse
+import matplotlib
+from torch import np
+import pylab as plt
+from joblib import Parallel, delayed
+import util
+import torch
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from collections import OrderedDict
+from config_reader import config_reader
+from scipy.ndimage.filters import gaussian_filter
+#parser = argparse.ArgumentParser()
+#parser.add_argument('--t7_file', required=True)
+#parser.add_argument('--pth_file', required=True)
+#args = parser.parse_args()
+
+"""Extracts pickle file of pytorch weights for use in tensorflow"""
+
+torch.set_num_threads(torch.get_num_threads())
+weight_name = './model/pose_model.pth'
+
+blocks = {}
+
+# find connection in the specified sequence, center 29 is in the position 15
+limbSeq = [[2,3], [2,6], [3,4], [4,5], [6,7], [7,8], [2,9], [9,10], \
+           [10,11], [2,12], [12,13], [13,14], [2,1], [1,15], [15,17], \
+           [1,16], [16,18], [3,17], [6,18]]
+
+# the middle joints heatmap correpondence
+mapIdx = [[31,32], [39,40], [33,34], [35,36], [41,42], [43,44], [19,20], [21,22], \
+          [23,24], [25,26], [27,28], [29,30], [47,48], [49,50], [53,54], [51,52], \
+          [55,56], [37,38], [45,46]]
+
+# visualize
+colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0], [0, 255, 0], \
+          [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255], \
+          [170, 0, 255], [255, 0, 255], [255, 0, 170], [255, 0, 85]]
+
+
+block0  = [{'conv1_1':[3,64,3,1,1]},{'conv1_2':[64,64,3,1,1]},{'pool1_stage1':[2,2,0]},{'conv2_1':[64,128,3,1,1]},{'conv2_2':[128,128,3,1,1]},{'pool2_stage1':[2,2,0]},{'conv3_1':[128,256,3,1,1]},{'conv3_2':[256,256,3,1,1]},{'conv3_3':[256,256,3,1,1]},{'conv3_4':[256,256,3,1,1]},{'pool3_stage1':[2,2,0]},{'conv4_1':[256,512,3,1,1]},{'conv4_2':[512,512,3,1,1]},{'conv4_3_CPM':[512,256,3,1,1]},{'conv4_4_CPM':[256,128,3,1,1]}]
+
+blocks['block1_1']  = [{'conv5_1_CPM_L1':[128,128,3,1,1]},{'conv5_2_CPM_L1':[128,128,3,1,1]},{'conv5_3_CPM_L1':[128,128,3,1,1]},{'conv5_4_CPM_L1':[128,512,1,1,0]},{'conv5_5_CPM_L1':[512,38,1,1,0]}]
+
+blocks['block1_2']  = [{'conv5_1_CPM_L2':[128,128,3,1,1]},{'conv5_2_CPM_L2':[128,128,3,1,1]},{'conv5_3_CPM_L2':[128,128,3,1,1]},{'conv5_4_CPM_L2':[128,512,1,1,0]},{'conv5_5_CPM_L2':[512,19,1,1,0]}]
+
+for i in range(2,7):
+    blocks['block%d_1'%i]  = [{'Mconv1_stage%d_L1'%i:[185,128,7,1,3]},{'Mconv2_stage%d_L1'%i:[128,128,7,1,3]},{'Mconv3_stage%d_L1'%i:[128,128,7,1,3]},{'Mconv4_stage%d_L1'%i:[128,128,7,1,3]},
+{'Mconv5_stage%d_L1'%i:[128,128,7,1,3]},{'Mconv6_stage%d_L1'%i:[128,128,1,1,0]},{'Mconv7_stage%d_L1'%i:[128,38,1,1,0]}]
+    blocks['block%d_2'%i]  = [{'Mconv1_stage%d_L2'%i:[185,128,7,1,3]},{'Mconv2_stage%d_L2'%i:[128,128,7,1,3]},{'Mconv3_stage%d_L2'%i:[128,128,7,1,3]},{'Mconv4_stage%d_L2'%i:[128,128,7,1,3]},
+{'Mconv5_stage%d_L2'%i:[128,128,7,1,3]},{'Mconv6_stage%d_L2'%i:[128,128,1,1,0]},{'Mconv7_stage%d_L2'%i:[128,19,1,1,0]}]
+
+def make_layers(cfg_dict):
+    layers = []
+    for i in range(len(cfg_dict)-1):
+        one_ = cfg_dict[i]
+        for k,v in one_.iteritems():
+            if 'pool' in k:
+                layers += [nn.MaxPool2d(kernel_size=v[0], stride=v[1], padding=v[2] )]
+            else:
+                conv2d = nn.Conv2d(in_channels=v[0], out_channels=v[1], kernel_size=v[2], stride = v[3], padding=v[4])
+                layers += [conv2d, nn.ReLU(inplace=True)]
+    one_ = cfg_dict[-1].keys()
+    k = one_[0]
+    v = cfg_dict[-1][k]
+    conv2d = nn.Conv2d(in_channels=v[0], out_channels=v[1], kernel_size=v[2], stride = v[3], padding=v[4])
+    layers += [conv2d]
+    return nn.Sequential(*layers)
+
+layers = []
+for i in range(len(block0)):
+    one_ = block0[i]
+    for k,v in one_.iteritems():
+        if 'pool' in k:
+            layers += [nn.MaxPool2d(kernel_size=v[0], stride=v[1], padding=v[2] )]
+        else:
+            conv2d = nn.Conv2d(in_channels=v[0], out_channels=v[1], kernel_size=v[2], stride = v[3], padding=v[4])
+            layers += [conv2d, nn.ReLU(inplace=True)]
+
+models = {}
+models['block0']=nn.Sequential(*layers)
+
+for k,v in blocks.iteritems():
+    models[k] = make_layers(v)
+
+class pose_model(nn.Module):
+    def __init__(self,model_dict,transform_input=False):
+        super(pose_model, self).__init__()
+        self.model0   = model_dict['block0']
+        self.model1_1 = model_dict['block1_1']
+        self.model2_1 = model_dict['block2_1']
+        self.model3_1 = model_dict['block3_1']
+        self.model4_1 = model_dict['block4_1']
+        self.model5_1 = model_dict['block5_1']
+        self.model6_1 = model_dict['block6_1']
+
+        self.model1_2 = model_dict['block1_2']
+        self.model2_2 = model_dict['block2_2']
+        self.model3_2 = model_dict['block3_2']
+        self.model4_2 = model_dict['block4_2']
+        self.model5_2 = model_dict['block5_2']
+        self.model6_2 = model_dict['block6_2']
+
+    def forward(self, x):
+        out1 = self.model0(x)
+
+        out1_1 = self.model1_1(out1)
+        out1_2 = self.model1_2(out1)
+        out2  = torch.cat([out1_1,out1_2,out1],1)
+
+        out2_1 = self.model2_1(out2)
+        out2_2 = self.model2_2(out2)
+        out3   = torch.cat([out2_1,out2_2,out1],1)
+
+        out3_1 = self.model3_1(out3)
+        out3_2 = self.model3_2(out3)
+        out4   = torch.cat([out3_1,out3_2,out1],1)
+
+        out4_1 = self.model4_1(out4)
+        out4_2 = self.model4_2(out4)
+        out5   = torch.cat([out4_1,out4_2,out1],1)
+
+        out5_1 = self.model5_1(out5)
+        out5_2 = self.model5_2(out5)
+        out6   = torch.cat([out5_1,out5_2,out1],1)
+
+        out6_1 = self.model6_1(out6)
+        out6_2 = self.model6_2(out6)
+
+        return out6_1,out6_2
+
+
+model = pose_model(models)
+model.load_state_dict(torch.load(weight_name))
+
+
+import tensorflow as tf
+import numpy as np
+import pickle
+#model.model0 is re_trained VGG
+weights0 = dict()
+#first layer of each branch, before re-concatenation
+weights1_1 = dict()
+weights1_2 = dict()
+
+for key in model.state_dict().keys():
+    model_key = key.replace("model", "")
+    is0 = model_key[0] == "0"
+    is1_1 = model_key[0:3] == "1_1"
+    is1_2 = model_key[0:3] == "1_2"
+    if is0 or is1_1 or is1_2:
+        weight = model.state_dict()[key]
+        format_weights = np.array(weight.tolist())
+        if is0:
+            weights0[key] = format_weights
+        elif is1_1:
+            weights1_1[key] = format_weights
+        elif is1_2:
+            weights1_2[key] = format_weights
+
+pickle.dump( weights0, open( "weights0.p", "wb" ) )
+pickle.dump( weights1_1, open( "weights1_1.p", "wb" ) )
+pickle.dump( weights1_2, open( "weights1_2.p", "wb" ) )

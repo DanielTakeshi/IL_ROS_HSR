1b5bef204eb4196df696bd714a62ab292acd6ed3
Chris Powers <chris.powers@berkeley.edu>
2017-07-13 14:39:43 -0700

    updated stat plotter

--- scripts/plot_cross_valid_stats.py
+++ scripts/plot_cross_valid_stats.py
@@ -39,8 +39,8 @@ from il_ros_hsr.p_pi.safe_corl.vgg_options import VGG_Options as options
 from il_ros_hsr.p_pi.safe_corl.com import Safe_COM as COM
 
 
-ITERATIONS = 200
-BATCH_SIZE = 200
+ITERATIONS = 1000
+BATCH_SIZE = 100
 
 
 Options = options()
@@ -49,34 +49,56 @@ Options = options()
 
 if __name__ == '__main__':
 
-    stats = pickle.load(open(Options.stats_dir+'all_cross_validate_stats.p','r'))
-    n = len(stats)
-    all_test_mins = []
+    all_stats = pickle.load(open(Options.stats_dir+'all_cross_validate_stats.p','r'))
+    fusion_stats = pickle.load(open(Options.stats_dir+'fusion_cross_validate_stats.p','r'))
 
-    fig = plt.figure(1)
-    ax = fig.add_subplot(111)
-    color = iter(plt.cm.rainbow(np.linspace(0,1,n)))
-    for stat in stats:
-        c = next(color)
-        ax.plot(stat['test_loss'],label=stat['type'], c = c)
-        print stat['type']
-        all_test_mins.append([stat['type'], min(stat['test_loss'])])
+    """
+    for each type, records average loss/time with/without fusion
+    """
+    with open("time.txt", "w") as time_file:
+        time_file.write("normal time    fusion time\n")
 
-    lgd = ax.legend(bbox_to_anchor=(1.05,1),loc=2,borderaxespad=0.)
-    all_test_mins.sort(key = lambda x: x[1])
-    print(all_test_mins)
+        for i, a_stat in enumerate(all_stats):
+            f_stat = [f in fusion_stats if f['type'] == a_stat['type']][0]
+            a_time = a_stat['avg_train_time']
+            f_time = f_stat['avg_train_time']
+            time_file.write("%8.2f  %8.2f\n" % a_time, f_time)
 
-    #plt.show()
-    fig.savefig("test_loss.png", bbox_extra_artists = (lgd,), bbox_inches='tight')
+            a_loss = a_stat['avg_test_loss']
+            f_loss = f-stat['avg_test_loss']
 
-    fig = plt.figure(2)
-    ax = fig.add_subplot(222)
-    color = iter(plt.cm.rainbow(np.linspace(0,1,n)))
-    for stat in stats:
-        c = next(color)
-        ax.plot(stat['train_loss'],label=stat['type'], c = c)
-        print stat['type']
+            fig = plt.figure(i)
+            ax = fig.add_subplot(i * 10)
 
-    lgd = ax.legend(bbox_to_anchor=(1.05,1),loc=2,borderaxespad=0.)
+            ax.plot(a_loss, label=a_stat['type'] + "_normal")
+            ax.plot(f_loss, label=f_stat['type'] + "_fusion")
+            lgd = ax.legend(bbox_to_anchor=(1.05,1),loc=2,borderaxespad=0.)
 
-    fig.savefig("train_loss.png", bbox_extra_artists = (lgd,), bbox_inches='tight')
+            fig.savefig(state['type'] + "_comparison.png", bbox_extra_artists = (lgd,), bbox_inches='tight')
+
+    """
+    within fusion/non-fusion, graphs each type together for test/train loss
+    """
+    j = 40
+    for stats, type_label in [(all_stats, "normal"), (fusion_stats, "fusion")]:
+        n = len(stats)
+
+        for loss in ["test_loss", "train_loss"]:
+            fig = plt.figure(j)
+            ax = fig.add_subplot(j * 10)
+            color = iter(plt.cm.rainbow(np.linspace(0,1,n)))
+            for stat in stats:
+                c = next(color)
+                ax.plot(stat["avg_" + loss],label=stat['type'], c = c)
+
+            lgd = ax.legend(bbox_to_anchor=(1.05,1),loc=2,borderaxespad=0.)
+
+            fig.savefig(type_label + "_" + loss + ".png", bbox_extra_artists = (lgd,), bbox_inches='tight')
+            j += 1
+        """ with fusion/non-fusion, record minimum loss reached for ranking """
+        test_mins = [(stat['type'], min(stat['avg_test_loss'])) for stat in stats]
+        test_mins.sort(key = lambda x: x[1])
+        with open(type_label + ".txt") as min_stats:
+            min_stats.write("type   min loss\n")
+            for cur_type, cur_min in test_mins:
+                min_stats.write(cur_type + "%3.5f\n" % cur_min)

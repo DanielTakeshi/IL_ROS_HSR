aa9ad7dc293ff93da99bfdd0659cf6348a743c87
DanielTakeshi <takeshidanny@gmail.com>
2018-08-20 09:07:00 -0700

    inspected my data, looks good

--- scripts/check_daniel_data.py
+++ scripts/check_daniel_data.py
@@ -1,6 +1,7 @@
 """Use this script to check for my dataset.
 
 I got these using `main/collect_data_bed_fast.py`.
+This is where I can get lots of HSR-based data on my end.
 """
 import cv2, pickle, sys, os
 import numpy as np
@@ -18,7 +19,6 @@ files = sorted(
     [join(ROLLOUTS1,x) for x in os.listdir(ROLLOUTS1)] +
     [join(ROLLOUTS2,x) for x in os.listdir(ROLLOUTS2)]
 )
-
 for f in files:
     print(f)
 
@@ -41,6 +41,9 @@ BLACK = (0,0,0)
 RED = (0,0,255)
 total = 0
 
+# ------------------------------------------------------------------------------
+all_x, all_y = [], []
+
 for fidx,ff in enumerate(files):
     # I saved in format: `{b,t}_grasp/data.pkl`. The pickle files are _lists_.
     data_list = pickle.load(open(ff,'rb'))
@@ -61,19 +64,32 @@ for fidx,ff in enumerate(files):
         d_path = os.path.join(IMG_PATH, 'file_{}_idx_{}_depth.png'.format(num1,num2))
         c_img = (data['c_img']).copy()
         d_img = (data['d_img']).copy()
+
         #cv2.patchNaNs(d_img, 0) # Shouldn't be needed with HSR data.
-        # NOTE!! This cutoff is HSR and situation dependent!
+        # NOTE NOTE NOTE!! This cutoff is HSR and situation dependent!
         d_img = depth_to_net_dim(d_img, cutoff=1400)
         assert d_img.shape == (480, 640, 3)
+
         pos = tuple(data['pose'])
+        all_x.append(pos[0])
+        all_y.append(pos[1])
 
         # Can overlay images with the marker position, which presumably is the target.
         cv2.circle(c_img, center=pos, radius=2, color=RED,   thickness=-1)
         cv2.circle(c_img, center=pos, radius=3, color=BLACK, thickness=1)
         cv2.circle(d_img, center=pos, radius=2, color=RED,   thickness=-1)
         cv2.circle(d_img, center=pos, radius=3, color=BLACK, thickness=1)
-
         cv2.imwrite(c_path, c_img)
         cv2.imwrite(d_path, d_img)
-
 print("total images: {}".format(total))
+
+# Let's get a scatter plot so we can see the distribution of data points.
+I = cv2.imread('scripts/imgs/daniel_data_example_file_15_idx_023_rgb.png')
+fig, ax = plt.subplots(1, 1, figsize=(8,6))
+ax.imshow(I, alpha=0.5)
+ax.scatter(all_x, all_y, color='black')
+ax.set_title('Scatter Plot of Data ({} Points)'.format(total), fontsize=title_size)
+ax.set_xlim([0, 640])
+ax.set_ylim([480, 0])
+plt.tight_layout()
+plt.savefig('scripts/imgs/daniel_data_scatter.png')

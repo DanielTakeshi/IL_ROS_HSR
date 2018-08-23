51a76ab763041b717096701d3b0dc5cf2f2601a1
DanielTakeshi <takeshidanny@gmail.com>
2018-08-22 19:40:24 -0700

    example deployment for other team to check

---
+++ main/example_load_network_grasp_only.py
@@ -0,0 +1,101 @@
+"""Example of how to load in and use both trained networks.
+"""
+import sys, os, IPython, cv2, time, thread, pickle
+import numpy as np
+import tensorflow as tf
+np.set_printoptions(suppress=True, precision=3)
+from os.path import join
+
+# We need a deployment-related config. This is where we put details
+# on the paths to the grasp and success network trained weights!
+import il_ros_hsr.p_pi.bed_making.config_bed as BED_CFG
+from fast_grasp_detect.detectors.grasp_detector import GDetector
+
+# Finally, load YOLO stem. Assumes we load pre-trained weights. It's the same
+# for both networks, so load before building to avoid duplicate TF names.
+from fast_grasp_detect.core.yolo_conv_features_cs import YOLO_CONV
+
+
+class Test():
+
+    def __init__(self):
+        """For testing how to deploy the policy.
+
+        As mentioned earlier, load in YOLO here and supply it to both of the
+        detectors. (For now, assume both use YOLO pre-trained, fixed stem.)
+        However, this requires an fg_cfg, which could be either the grasp or the
+        success one ... i.e., the cfg we used for training. So, if both used the
+        YOLO net, they better use the same cfg! If only one did, then we'll use
+        that one ... a lot of manual work, unfortunately.
+
+        BTW, this will create three TensorFlow sessions, assuming we're sharing
+        the YOLO stem (which is one TF session) among the two policies.
+        """
+        g_cfg = BED_CFG.GRASP_CONFIG
+        self.yc = YOLO_CONV(options=g_cfg)
+        self.yc.load_network()
+        self.g_detector = GDetector(g_cfg, BED_CFG, yc=self.yc)
+
+
+    def test_grasp(self, all_cv_files):
+        """Test on all the (grasping-related) data in our CV files.
+
+        IMPORTANT: These have ALREADY had the depth image pre-processed
+        before the call to `g_detector.predict(d_img)`.
+
+        If you have raw depth images you want to try, you need to do:
+
+            (1) convert NaN to 0 using cv2.patchNaNs(img, 0.0)
+            (2) run the preprocessing in the depth preprocessing script
+
+        For (2) use:
+
+            d_img = depth_to_net_dim(d_img, robot='Fetch')
+
+        fast_grasp_detect.data_aug.depth_preprocess
+
+        Note that as of August 22, the API has the robot exposed, but keeps
+        the cutoff hidden, to reduce risks of different cutoffs.
+        """
+        L2_results = []
+
+        for test_list in all_cv_files:
+            with open(test_list, 'r') as f:
+                data = pickle.load(f)
+            print("loaded test data: {} (length {})".format(test_list, len(data)))
+
+            for idx,item in enumerate(data):
+                if BED_CFG.GRASP_CONFIG.USE_DEPTH:
+                    d_img = np.copy(item['d_img'])
+                    result = self.g_detector.predict(d_img)
+                else:
+                    c_img = np.copy(item['c_img'])
+                    result = self.g_detector.predict(c_img)
+                result = np.squeeze( np.array(result) ) # batch size is 1
+                targ = item['pose']
+                L2 = np.sqrt( (result[0]-targ[0])**2 + (result[1]-targ[1])**2 )
+                print("  prediction {} for {}, pixel L2 {:.1f}".format(result, idx, L2))
+                L2_results.append(L2)
+                #time.sleep(0.5) # pause the 'video' of images :-)
+        
+        print("L2s: {:.1f} +/- {:.1f}".format(np.mean(L2_results), np.std(L2_results)))
+
+
+def get_variables():
+    print("")
+    variables = tf.trainable_variables()
+    numv = 0
+    for vv in variables:
+        numv += np.prod(vv.shape)
+        print(vv)
+    print("\nNumber of parameters: {}".format(numv))
+
+
+if __name__ == "__main__":
+    TestBed = Test()
+    PATH_GRASP   = '/nfs/diskstation/seita/bed-make/cache_h_v03/'
+    all_cv_files_grasp   = sorted(
+            [join(PATH_GRASP,x) for x in os.listdir(PATH_GRASP) if 'cv_' in x]
+    )
+    TestBed.test_grasp(all_cv_files_grasp)
+    print("\nDone!")
--- src/il_ros_hsr/p_pi/bed_making/config_bed.py
+++ src/il_ros_hsr/p_pi/bed_making/config_bed.py
@@ -37,10 +37,10 @@ assert VIEW_MODE in ['standard', 'close']
 # --- GRASP NETWORK ---
 # ---------------------
 # When deploying, we need to load in a config (text) file and a network.
-g_data_name = 'cache_d_v01'
+g_data_name = 'cache_h_v03'
 g_head_name = 'grasp_1_img_depth_opt_adam_lr_0.0001_L2_0.0001_kp_1.0_cv_False'
-g_ckpt_name = '08_22_13_09_05_save.ckpt-3000'
-g_conf_name = 'config_2018_08_22_13_06.txt'
+g_ckpt_name = '08_22_19_12_59_save.ckpt-3000'
+g_conf_name = 'config_2018_08_22_19_10.txt'
 GRASP_NET_PATH  = join(ROOT_DIR, 'grasp', g_data_name, g_head_name, g_ckpt_name)
 GRASP_CONF_PATH = join(ROOT_DIR, 'grasp', g_data_name, g_head_name, g_conf_name)
 assert 'save.ckpt' in g_ckpt_name and 'config' in g_conf_name
@@ -62,33 +62,33 @@ for line in g_content:
     else:
         setattr(GRASP_CONFIG, attr, convert(value))
 
-# -----------------------
-# --- SUCCESS NETWORK ---
-# -----------------------
-s_data_name = 'cache_d_v01_success'
-s_head_name = 'success_1_img_depth_opt_adam_lr_0.0001_L2_0.0001_kp_1.0_cv_False'
-s_ckpt_name = '08_22_12_58_22_save.ckpt-1000'
-s_conf_name = 'config_2018_08_22_12_57.txt'
-SUCC_NET_PATH  = join(ROOT_DIR, 'success', s_data_name, s_head_name, s_ckpt_name)
-SUCC_CONF_PATH = join(ROOT_DIR, 'success', s_data_name, s_head_name, s_conf_name)
-assert 'save.ckpt' in s_ckpt_name and 'config' in s_conf_name
-
-# Set SUCC_CONFIG to be the same as what we had during neural net training.
-SUCC_CONFIG = BuildConfig()
-with open(SUCC_CONF_PATH, 'r') as f:
-    s_content = f.readlines()
-s_content = [x.strip() for x in s_content]
-for line in s_content:
-    line = line.split(':')
-    assert len(line) == 2
-    attr = line[0].strip()
-    value = line[1].strip()
-    if value == 'True':
-        setattr(SUCC_CONFIG, attr, True)
-    if value == 'False':
-        setattr(SUCC_CONFIG, attr, False)
-    else:
-        setattr(SUCC_CONFIG, attr, convert(value))
+### # -----------------------
+### # --- SUCCESS NETWORK ---
+### # -----------------------
+### s_data_name = 'cache_d_v01_success'
+### s_head_name = 'success_1_img_depth_opt_adam_lr_0.0001_L2_0.0001_kp_1.0_cv_False'
+### s_ckpt_name = '08_22_12_58_22_save.ckpt-1000'
+### s_conf_name = 'config_2018_08_22_12_57.txt'
+### SUCC_NET_PATH  = join(ROOT_DIR, 'success', s_data_name, s_head_name, s_ckpt_name)
+### SUCC_CONF_PATH = join(ROOT_DIR, 'success', s_data_name, s_head_name, s_conf_name)
+### assert 'save.ckpt' in s_ckpt_name and 'config' in s_conf_name
+### 
+### # Set SUCC_CONFIG to be the same as what we had during neural net training.
+### SUCC_CONFIG = BuildConfig()
+### with open(SUCC_CONF_PATH, 'r') as f:
+###     s_content = f.readlines()
+### s_content = [x.strip() for x in s_content]
+### for line in s_content:
+###     line = line.split(':')
+###     assert len(line) == 2
+###     attr = line[0].strip()
+###     value = line[1].strip()
+###     if value == 'True':
+###         setattr(SUCC_CONFIG, attr, True)
+###     if value == 'False':
+###         setattr(SUCC_CONFIG, attr, False)
+###     else:
+###         setattr(SUCC_CONFIG, attr, convert(value))
 
 
 # --- Other stuff which I don't need to look at often ---

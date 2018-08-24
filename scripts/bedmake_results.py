137f919c82a3c4d9a18cbfa632ef6a1fe7d7cf63
DanielTakeshi <takeshidanny@gmail.com>
2018-08-23 21:57:07 -0700

    bedmake results for 3 trajectors. :-) get more tomorrow then send draft v03

--- scripts/bedmake_results.py
+++ scripts/bedmake_results.py
@@ -36,30 +36,65 @@ error_fc = 'blue'
 def analyze_time(stats):
     """For analyzing time. These should be in seconds unless otherwise stated.
     """
-    print("\nTimes for moving to other side:")
-    move_t = np.array( stats['move_times'] )
-    print(move_t)
-    print("\nTimes for executing grasps:")
-    grasp_t = np.array( stats['grasp_times'] )
-    print(grasp_t)
 
+    # Robot motion to another side. Very slow. Should include both sides.
+    move_t = []
+    for stats_l in stats['move_times']:
+        for tt in stats_l:
+            move_t.append(tt)
+    print("\nTimes for moving to other side, length: {}".format(len(move_t)))
+    print("{:.3f}\pm {:.1f}".format(np.mean(move_t), np.std(move_t)))
+
+    # Robot grasp execution. Also slow, can be highly variable.
+    grasp_t = []
+    for stats_l in stats['grasp_times']:
+        for tt in stats_l:
+            grasp_t.append(tt)
+    print("\nTimes for executing grasps, length: {}".format(len(grasp_t)))
+    print("{:.3f}\pm {:.1f}".format(np.mean(grasp_t), np.std(grasp_t)))
+
+    # Now times for grasping and success net. These are quick.
     grasp_net_t = []
     success_net_t = []
-    result = stats['result_0'] # only one rollout here
+    lengths = []
+    idx = 0
+    key = 'result_{}'.format(idx)
 
-    for i,info in enumerate(result):
-        if result[i]['type'] == 'grasp':
-            grasp_net_t.append( result[i]['g_net_time'] )
-        else:
-            assert result[i]['type'] == 'success'
-            success_net_t.append( result[i]['s_net_time'] )
+    while key in stats:
+        # ----------------------------------------------------------------------
+        # Analyze one rollout (i.e., `stats[key]`) at a time.
+        # This is a list, where at each index, result[i] is a dict w/relevant
+        # info. Also, I directly use length because I took the last dict, the
+        # 'final_dict', out before hand to analyze separately.
+        # ----------------------------------------------------------------------
+        result = stats[key]
+        for i,info in enumerate(result):
+            if result[i]['type'] == 'grasp':
+                grasp_net_t.append( result[i]['g_net_time'] )
+            else:
+                assert result[i]['type'] == 'success'
+                success_net_t.append( result[i]['s_net_time'] )
+        idx += 1
+        key = 'result_{}'.format(idx)
+        lengths.append(len(result))
+    assert len(grasp_net_t) == len(grasp_net_t)
 
+    # For the grasp/success nets, if they're the same architecture, prob combine them
     grasp_net_t = np.array(grasp_net_t)
     success_net_t = np.array(success_net_t)
-    print("\ngrasp_net_t:\n{}".format(grasp_net_t))
-    print("{:.3f} +/- {:.3f}".format(np.mean(grasp_net_t), np.std(grasp_net_t)))
-    print("\nsuccess_net_t:\n{}".format(success_net_t))
-    print("{:.3f} +/- {:.3f}".format(np.mean(success_net_t), np.std(success_net_t)))
+    print("\ngrasp_net_t.shape: {}".format(grasp_net_t.shape))
+    print("{:.3f}\pm {:.1f}".format(np.mean(grasp_net_t), np.std(grasp_net_t)))
+    print("\nsuccess_net_t.shape: {}".format(success_net_t.shape))
+    print("{:.3f}\pm {:.1f}".format(np.mean(success_net_t), np.std(success_net_t)))
+    all_net_t = np.concatenate((grasp_net_t,success_net_t))
+    print("\nboth networks, data shape: {}".format(all_net_t.shape))
+    print("{:.3f}\pm {:.1f}".format(np.mean(all_net_t), np.std(all_net_t)))
+
+    # Another thing, trajectory _lengths_.
+    print("\nlengths.mean(): {}".format(np.mean(lengths)))
+    print("lengths.std():  {}".format(np.std(lengths)))
+    print("lengths.min():  {}".format(np.min(lengths)))
+    print("lengths.max():  {}".format(np.max(lengths)))
 
 
 def analyze_preds(stats):
